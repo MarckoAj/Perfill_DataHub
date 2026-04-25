cca76b94213ad0ea43d3891c54187e517dde2f36
MarckoAj <96995466+MarckoAj@users.noreply.github.com>
2026-04-25 04:56:59 -0300

    chore: adicionar suporte a SSL na conexão com o banco para Aiven/Render

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,13 +4,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = process.env.DB_URL
-  ? mysql.createPool({ uri: process.env.DB_URL, waitForConnections: true, connectionLimit: 10, queueLimit: 0 })
+  ? mysql.createPool({ 
+      uri: process.env.DB_URL, 
+      ssl: { rejectUnauthorized: false },
+      waitForConnections: true, 
+      connectionLimit: 10, 
+      queueLimit: 0 
+    })
   : mysql.createPool({
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
+      ssl: { rejectUnauthorized: false },
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0
